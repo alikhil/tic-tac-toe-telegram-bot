41f1a00223193c0cfd19a3f476c8a25bbc781828
Alik Khilazhev <alikhil@mail.ru>
2016-06-27 11:20:41 +0300

    Updates emojis.

--- emoji.py
+++ emoji.py
@@ -1,6 +1,6 @@
 # Small custom emoji collection
 class Emoji:
 	"""UTF-8 emojis"""
-	HEAVY_MULTIPLICATION_X = '✖'.encode('unicode_escape')
-	HEAVY_LARGE_CIRCLE = u'⭕'.encode('unicode_escape')
-	LIKE = u'👍'.encode('unicode_escape')
+	HEAVY_MULTIPLICATION_X = u'\u2716'
+	HEAVY_LARGE_CIRCLE = u'\u2b55'
+	LIKE = u'\U0001f44d'
